b77ed28d381f13cdcbc8e38ecf932e957e1bf339
MD MANIRUZZAMAN <168886897+mzamandvlpr@users.noreply.github.com>
2024-05-25 08:22:24 -0400

    Add files via upload

---
+++ IfElseStatement.java
@@ -0,0 +1,18 @@
+package javabasiccodingmay24;
+
+public class IfElseStatement {
+    public static void main(String[] args)
+    {
+           boolean A = true;
+        if(A)
+
+        {
+            System.out.println("B");
+
+        }
+         else{
+            System.out.println("C");
+        }
+
+    }
+}
